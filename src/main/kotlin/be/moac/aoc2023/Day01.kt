4cec796263d78c0eac0af8d83cda37d900ad4ec2
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2023-12-02 15:58:24 +0100

    Day one (wa was da)

--- src/main/kotlin/be/moac/aoc2023/Day01.kt
+++ src/main/kotlin/be/moac/aoc2023/Day01.kt
@@ -1,5 +1,6 @@
 package be.moac.aoc2023
 
+import be.moac.aoc2023.Day01.first
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -12,10 +13,66 @@ fun main() {
 
 object Day01 {
 
-    infix fun partOne(input: List<String>): Long = 0L
+    infix fun partOne(input: List<String>): Long =
+        input
+            .mapIndexed { index, s -> "${s.firstDigit()}${s.lastDigit()}".toLong().also { println("$index - $it") } }
+            .sum()
+
+    infix fun partTwo(input: List<String>): Long =
+        input
+            .mapIndexed { index, s -> "${s.first()}${s.last()}".toLong().also { println("$index - $it,") } }
+            .sum()
+
+    fun String.firstDigit(): Char = this.first { it.isDigit() }
+    fun String.lastDigit(): Char = this.last { it.isDigit() }
+
+    private val digits = mapOf(
+        "one" to 1,
+        "two" to 2,
+        "three" to 3,
+        "four" to 4,
+        "five" to 5,
+        "six" to 6,
+        "seven" to 7,
+        "eight" to 8,
+        "nine" to 9,
+    )
 
 
-    infix fun partTwo(input: List<String>): Long = 0L
+    fun String.first(): Int {
+        return digits.entries
+            .map {
+                val a = this.indexOf(it.key)
+                val b = this.indexOf("${it.value}")
+                when {
+                    a == -1 -> b
+                    b == -1 -> a
+                    a < b -> a
+                    else -> b
+                } to it
+            }
+            .filterNot { it.first == -1 }
+            .minByOrNull { it.first }?.second?.value!!
+    }
 
+    fun String.last(): Int {
+        return digits.entries
+            .map {
+                val a = this.lastIndexOf(it.key)
+                val b = this.lastIndexOf("${it.value}")
+                when {
+                    a == -1 -> b
+                    b == -1 -> a
+                    a > b -> a
+                    else -> b
+                } to it
 
+            }
+            .filterNot {
+                it.first == -1
+            }
+            .maxByOrNull {
+                it.first
+            }?.second?.value!!
+    }
 }
