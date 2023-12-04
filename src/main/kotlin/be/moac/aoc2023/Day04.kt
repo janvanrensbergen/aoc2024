547d18c0bdcb2afaeda200539021163556293f21
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2023-12-04 14:33:01 +0100

    Day four part two

--- src/main/kotlin/be/moac/aoc2023/Day04.kt
+++ src/main/kotlin/be/moac/aoc2023/Day04.kt
@@ -1,5 +1,6 @@
 package be.moac.aoc2023
 
+import be.moac.aoc2023.Day02.parse
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -16,8 +17,15 @@ object Day04 {
         input.map { it.parse() }.sumOf { it.points }
 
 
-    infix fun partTwo(input: List<String>): Long = 0
-
+    infix fun partTwo(input: List<String>): Int  {
+        val cards = input.map { it.parse() }
+        return cards.mapIndexed { index, card ->
+            (index + 1 .. index +  card.result.size)
+                .forEach { i -> if (i < cards.size) cards[i].inc(card.numberOfCopies) }
+            card
+        }
+        .sumOf { it.numberOfCopies }
+    }
 
     fun String.parse(): Card {
         val (winning, myNumbers) = this.split(":").last().split("|")
@@ -27,15 +35,17 @@ object Day04 {
         )
     }
 
-    data class Card(val winningNumbers: List<Long>, val numbers: List<Long>) {
+    data class Card(val winningNumbers: List<Long>, val numbers: List<Long>, var numberOfCopies: Int = 1) {
         val result: Set<Long> get() = numbers.intersect(winningNumbers.toSet())
 
         val points: Long get() = result.fold(0) { acc, _ -> when {
             acc == 0L -> 1
             else -> acc * 2
         } }
+        fun inc(value: Int) {
+            this.numberOfCopies += value
+        }
     }
-
 }
 
 private val regex = "\\b\\d+\\b".toRegex()
