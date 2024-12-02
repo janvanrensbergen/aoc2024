2a61556b4ae5f0a62984ab28fd54d461735cc1a8
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2024-12-02 11:28:14 +0100

    Day 01 - part 1

--- src/main/kotlin/be/moac/aoc2024/Day01.kt
+++ src/main/kotlin/be/moac/aoc2024/Day01.kt
@@ -6,14 +6,28 @@ import kotlin.time.ExperimentalTime
 fun main() {
     val input: List<String> = "/day01_input.txt".readLines { it }
 
-    println("Part one: ${timed { Day01 partOne input }}")
-    println("Part two: ${timed { Day01 partTwo input }}")
+    println("Part one: ${timed(1) { Day01 partOne input }}")
+//    println("Part two: ${timed { Day01 partTwo input }}")
 }
 
 object Day01 {
 
-    infix fun partOne(input: List<String>): Long = 0L
+    infix fun partOne(input: List<String>): Long =
+        input.parse().sumOf {
+            if(it.second > it.first) it.second - it.first else it.first - it.second
+        }
+
     infix fun partTwo(input: List<String>): Long = 0L
+
+    private fun List<String>.parse(): List<Pair<Long, Long>> =
+        this.filterNot { it.isBlank() }
+        .map { line ->
+            val split = line.split("   ")
+            split[0].toLong() to split[1].toLong()
+        }
+        .fold(listOf<Long>() to listOf<Long>()) { acc, current ->
+            (acc.first + current.first).sorted() to (acc.second + current.second).sorted()
+        }.let { it.first.zip(it.second) }
 
 
 }
--- src/test/kotlin/be/moac/aoc2024/Day01Test.kt
+++ src/test/kotlin/be/moac/aoc2024/Day01Test.kt
@@ -1,5 +1,6 @@
 package be.moac.aoc2024
 
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
 class Day01Test {
@@ -7,7 +8,16 @@ class Day01Test {
 
     @Test
     fun `that first digit is found`() {
+          val input = """
+              3   4
+              4   3
+              2   5
+              1   3
+              3   9
+              3   3
+          """.trimIndent().lines()
 
+        Assertions.assertThat(Day01 partOne input).isEqualTo(11L)
     }
 
     @Test
