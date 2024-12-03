fb768df0b88d6864aa61d849330a10e0ce13370e
Jan Van Rensbergen <jan.vanrensbergen@gmail.com>
2024-12-03 10:07:17 +0100

    Day 03 - Part 2

--- src/main/kotlin/be/moac/aoc2024/Day03.kt
+++ src/main/kotlin/be/moac/aoc2024/Day03.kt
@@ -1,5 +1,6 @@
 package be.moac.aoc2024
 
+import kotlin.text.RegexOption.DOT_MATCHES_ALL
 import kotlin.time.ExperimentalTime
 
 @OptIn(ExperimentalTime::class)
@@ -7,17 +8,27 @@ fun main() {
     val input: String = "/day03_input.txt".readResource()
 
     println("Part one: ${timed { Day03 partOne input }}")
-//    println("Part two: ${timed { Day03 partTwo input }}")
+    println("Part two: ${timed { Day03 partTwo input }}")
 }
 
 object Day03 {
     infix fun partOne(input: String): Long =
-        input.parse().sumOf { it.first * it.second }
+        input.parse().sum()
 
-    infix fun partTwo(input: String): Long = 0L
+    infix fun partTwo(input: String): Long =
+        input.removeDisabledParts().parse().sum()
 
 
-    private fun String.parse() =
+    private fun List<Pair<Long, Long>>.sum() =
+        this.sumOf { it.first * it.second }
+
+    private fun String.removeDisabledParts(): String =
+        when(val match = patternRemove.find(this)) {
+            null -> this
+            else -> this.replace(match.value, "").removeDisabledParts()
+        }
+
+    private fun String.parse(): List<Pair<Long, Long>> =
         pattern.findAll(this).map {
             it.groups[1]?.value?.toLong()!! to it.groups[2]?.value?.toLong()!!
         }.toList()
@@ -25,3 +36,4 @@ object Day03 {
 }
 
 private val pattern = Regex("mul\\((\\d+),(\\d+)\\)")
+private val patternRemove = Regex("don't\\(\\).*?do\\(\\)", DOT_MATCHES_ALL)
--- src/test/kotlin/be/moac/aoc2024/Day03Test.kt
+++ src/test/kotlin/be/moac/aoc2024/Day03Test.kt
@@ -2,10 +2,10 @@ package be.moac.aoc2024
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
 
 class Day03Test {
+
+
 
     /**
      * For example, consider the following section of corrupted memory:
@@ -13,17 +13,24 @@ class Day03Test {
      * xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
      * Only the four highlighted sections are real mul instructions. Adding up the result of each instruction produces 161 (2*4 + 5*5 + 11*8 + 8*5).
      */
-
     @Test
     fun `part 1`() {
         val input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-
         assertThat(Day03 partOne input).isEqualTo(161L)
     }
 
+
+    /**
+     * xmul(2,4)&mul[3,7]!^don't()_mul(5,5)don't()+mul(32,64](mul(11,8)undo()?mul(8,5))
+     * This corrupted memory is similar to the example from before, but this time the mul(5,5) and mul(11,8) instructions are disabled because there is a don't() instruction before them.
+     * The other mul instructions function normally, including the one at the end that gets re-enabled by a do() instruction.
+     *
+     * This time, the sum of the results is 48 (2*4 + 8*5).
+     */
     @Test
     fun `part 2`() {
-
+        val input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)don't()+mul(32,64](mul(11,8)undo()?mul(8,5))"
+        assertThat(Day03 partTwo input).isEqualTo(48L)
     }
 
 }
